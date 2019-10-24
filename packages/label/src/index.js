75709593b84dfc4007acf95e76f169331a14c880
Guillem Aldeguer Lima <galdeguer@userzoom.com>
2019-10-24 16:56:35 +0200

    fix: fix·

--- packages/label/src/index.js
+++ packages/label/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 
-
 const Label = ({ className, onClick = () => {}, children }) => (
   <label className={`gz-label ${className}`} onClick={onClick}>
     Label - {children}
